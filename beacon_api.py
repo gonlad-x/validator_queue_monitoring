16ce12345f4d9f45b784a0b8ffd6808a28b50153
gonlad-x <mathildefroissart@gmail.com>
2023-03-19 09:54:22 +0100

    update script to generate html file for vercel

--- beacon_api.py
+++ beacon_api.py
@@ -1,30 +1,59 @@
 import requests
-import json
-
-BASE_URL = "https://beaconcha.in/api/v1"
-
-def get_validator_queue():
-    response = requests.get(f"{BASE_URL}/validators/queue")
-    data = response.json()
-    return data
+import os
 
 def estimate_waiting_time():
-    validator_queue = get_validator_queue()
-    pending_validators = validator_queue["data"]["beaconchain_entering"]
-    active_validators = validator_queue["data"]["validatorscount"]
-    activation_rate = 4
-    epoch_length_minutes = 6.4
+    response = requests.get('https://beaconcha.in/api/v1/validators/queue')
+    data = response.json()
+    pending_validators = data["data"]["beaconchain_entering"]
+    active_validators = data["data"]["validatorscount"]
+    activation_rate = 4  # 4 validators per epoch (every 6.4 minutes)
 
-    queue_epochs = pending_validators / activation_rate
-    queue_time_minutes = queue_epochs * epoch_length_minutes
+    waiting_time_minutes = (pending_validators / activation_rate) * 6.4
+    waiting_time_hours = waiting_time_minutes // 60
+    waiting_time_minutes = round(waiting_time_minutes % 60)
 
-    hours = int(queue_time_minutes // 60)
-    minutes = int(queue_time_minutes % 60)
+    return waiting_time_hours, waiting_time_minutes, pending_validators, active_validators
 
-    print(f"Number of active validators: {active_validators}")
-    print(f"Number of pending validators: {pending_validators}")
-    print(f"Estimated waiting time before a new validator is activated: {hours} hours {minutes} minutes")
+def generate_html(waiting_time_hours, waiting_time_minutes, pending_validators, active_validators):
+    html_content = f"""<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Validator Queue</title>
+    <style>
+        body {{
+            font-family: Arial, sans-serif;
+            max-width: 800px;
+            margin: 0 auto;
+            background-color: #f4f4f4;
+            padding: 1rem;
+        }}
 
-if __name__ == "__main__":
-    estimate_waiting_time()
+        h1 {{
+            color: #333;
+            font-size: 2rem;
+        }}
 
+        p {{
+            font-size: 1.1rem;
+            color: #666;
+            margin-bottom: 1.5rem;
+        }}
+    </style>
+</head>
+<body>
+    <h1>Ethereum 2.0 Validator Queue</h1>
+    <p>Estimated waiting time for new validators: {waiting_time_hours} hours and {waiting_time_minutes} minutes</p>
+    <p>Pending validators: {pending_validators}</p>
+    <p>Active validators: {active_validators}</p>
+</body>
+</html>"""
+
+    with open("index.html", "w") as f:
+        f.write(html_content)
+
+
+waiting_time_hours, waiting_time_minutes, pending_validators, active_validators = estimate_waiting_time()
+generate_html(waiting_time_hours, waiting_time_minutes, pending_validators, active_validators)
